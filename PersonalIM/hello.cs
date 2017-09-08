0a74566d2da9acdca8f697c6f51a36ebbf300ce3
Ryan <ryankurz123@gmail.com>
2017-09-09 09:29:22 +1000

    Created Hello World

---
+++ PersonalIM/hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PersonalIM
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello Mono World");
+        }
+        
+    }
+}
