811fe5ada031a74bf362f6ea7f76871da4536b65
Ryan <ryankurz123@gmail.com>
2017-09-13 11:00:28 +1000

    Adding some stuff

--- PersonalIM/client.cs
+++ PersonalIM/client.cs
@@ -1,8 +1,18 @@
-﻿namespace PersonalIM
+﻿
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace PersonalIM
 {
-    public class Client
+    public partial class Form1 : Form
     {
-        
-        
+        //Create the client
+        private System.Net.Sockets.TcpClient _client = new TcpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
     }
 }
