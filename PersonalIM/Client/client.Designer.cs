19f11b7e9bb889fac52d1cfc4df437ab16e2ab67
Ryan <ryankurz123@gmail.com>
2017-09-14 10:35:52 +1000

    Visually working but not technically

---
+++ PersonalIM/Client/client.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows.Forms;
+
+namespace Client
+{
+    partial class client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 166);
+            this.textBox1.MinimumSize = new System.Drawing.Size(250, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 60);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 73);
+            this.textBox2.MinimumSize = new System.Drawing.Size(100, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 50);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "client";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- PersonalIM/Client/client.cs
+++ PersonalIM/Client/client.cs
@@ -1,38 +1,44 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PersonalIM
+namespace Client
 {
-    public partial class Form1 : Form
+    public partial class client : Form
     {
-        //Create the client
-        private System.Net.Sockets.TcpClient client = new TcpClient(); 
-        public Form1()
+        private System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+        public client()
         {
-            
             InitializeComponent();
-            
         }
-        
+
         private void Form1_Load(object sender, EventArgs e)
 
         {
 
             msg("Client Started");
 
-            client.Connect("127.0.0.1", 8888);
+            clientSocket.Connect("127.0.0.1", 8888);
 
             label1.Text = "Client Socket Program - Server Connected ...";
 
         }
-        
+
+
+
         private void button1_Click(object sender, EventArgs e)
 
         {
 
-            NetworkStream serverStream = client.GetStream();
+            NetworkStream serverStream = clientSocket.GetStream();
 
             byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
 
@@ -44,7 +50,7 @@ namespace PersonalIM
 
             byte[] inStream = new byte[10025];
 
-            serverStream.Read(inStream, 0, (int)client.ReceiveBufferSize);
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
 
             string returndata = System.Text.Encoding.ASCII.GetString(inStream);
 
@@ -64,6 +70,7 @@ namespace PersonalIM
 
             textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
 
-        } 
+        }
+
     }
 }
